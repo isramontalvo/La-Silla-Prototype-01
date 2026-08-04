db19b3c58bd8c0266eed7c1ffae4157801f71a9b
isramontalvo <isra.montalvo@gmail.com>
2026-08-03 18:20:09 -0600

    Keep player inside game area

--- player.gd
+++ player.gd
@@ -12,3 +12,18 @@ func _physics_process(_delta: float) -> void:
 
 	velocity = direction * speed
 	move_and_slide()
+
+	var screen_size := get_viewport_rect().size
+	var margin := 20.0
+
+	global_position.x = clampf(
+		global_position.x,
+		margin,
+		screen_size.x - margin
+	)
+
+	global_position.y = clampf(
+		global_position.y,
+		margin,
+		screen_size.y - margin
+	)
