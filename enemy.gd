3f393cb1c71c7beefd007b507cf2b5fd0098a4e4
isramontalvo <isra.montalvo@gmail.com>
2026-08-03 18:15:06 -0600

    Add enemy chase and player defeat

---
+++ enemy.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@export var speed: float = 100.0
+
+@onready var player: CharacterBody2D = $"../Player"
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _physics_process(delta: float) -> void:
+	var direction := global_position.direction_to(player.global_position)
+	global_position += direction * speed * delta
+
+func _on_body_entered(body: Node2D) -> void:
+	if body == player:
+		get_tree().reload_current_scene()
