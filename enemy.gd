5979697f8027ea83363753335b068b345de7b684
isramontalvo <isra.montalvo@gmail.com>
2026-09-03 19:47:16 -0600

    Add four-direction Grandpa chase animation

--- enemy.gd
+++ enemy.gd
@@ -9,11 +9,18 @@ var avoidance_time: float = 0.0
 var chase_active: bool = false
 
 @onready var player: CharacterBody2D = $"../Player"
+@onready var sprite: AnimatedSprite2D = $Sprite2D
+
+
+func _ready() -> void:
+	sprite.animation = "down"
+	sprite.stop()
 
 
 func _physics_process(delta: float) -> void:
 	if not chase_active:
 		velocity = Vector2.ZERO
+		sprite.stop()
 		return
 
 	var direction := global_position.direction_to(player.global_position)
@@ -23,6 +30,8 @@ func _physics_process(delta: float) -> void:
 		velocity = avoidance_direction * speed
 	else:
 		velocity = direction * speed
+
+	update_run_animation(velocity.normalized())
 
 	move_and_slide()
 
@@ -45,6 +54,24 @@ func _physics_process(delta: float) -> void:
 
 		avoidance_time = 0.45
 		break
+
+
+func update_run_animation(direction: Vector2) -> void:
+	var animation_name: String
+
+	if abs(direction.x) > abs(direction.y):
+		if direction.x > 0.0:
+			animation_name = "right"
+		else:
+			animation_name = "left"
+	else:
+		if direction.y > 0.0:
+			animation_name = "down"
+		else:
+			animation_name = "up"
+
+	if sprite.animation != animation_name or not sprite.is_playing():
+		sprite.play(animation_name)
 
 
 func start_chase() -> void:
