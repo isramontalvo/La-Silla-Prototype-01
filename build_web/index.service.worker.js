24f224c9bcc3c9776c85731b5eb88851fb54df09
isramontalvo <isra.montalvo@gmail.com>
2026-09-10 19:47:27 -0600

    Build Mouse Rush 1.0 web release candidate

--- build_web/index.service.worker.js
+++ build_web/index.service.worker.js
@@ -4,9 +4,9 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787544001|11322646954';
+const CACHE_VERSION = '1789090731|2316281';
 /** @type {string} */
-const CACHE_PREFIX = 'La Silla Prototy-sw-cache-';
+const CACHE_PREFIX = 'Mouse Rush!-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';
